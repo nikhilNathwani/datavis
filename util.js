16573e51ca26ece4ba7fd9205d47dc102f068752
nikhilNathwani <njn27@cornell.edu>
2014-06-20 17:16:49 -0400

    Moved setOrbit function to util.js

--- util.js
+++ util.js
@@ -28,21 +28,21 @@ function teamText(d) {
 //zoom or un-zoom informational circle
 function resize(group) {
     //grab group's circle and text
-    circ= group.select("circle");
-    txt= group.select("text")
+    circ= group.select("circle.orbit");
+    txt= group.select("text.orbit")
     
     //clicked to zoom circle
     if (circ.attr("r") != zoomRad) {
-        gs= svg.selectAll("g")
+        gs= svg.selectAll("g.orbit")
         
         //zoom out all zoomed circles 
-        gs.selectAll("circle")
+        gs.selectAll("circle.orbit")
             .transition().delay(0).duration(500).attr("r", function(d) {
                     return d.wins*10+10;
                 });
             
         //hide all visible circle text
-        gs.selectAll("text")
+        gs.selectAll("text.orbit")
             .transition().delay(0).duration(500).attr("visibility","hidden");
         
         //move this group's circle and text to fronr
@@ -62,4 +62,65 @@ function resize(group) {
         });
         txt.transition().delay(200).duration(500).attr("visibility","hidden");
     }
+}
+
+function setTeamOrbit(dat) {
+    var name= dat.team;
+    var neighbors= dat.neighbors;
+    
+    x= getDistBounds(neighbors);
+    max= x[0];
+    min= x[1];
+    diff= max-min; //used when rescaling neighbor distance
+    
+
+    //set properties and event handlers of each circle in orbit
+    orbit= svg.selectAll("g.orbit")
+            .data(neighbors)
+        
+    orbit.select("circle.orbit")
+        .attr("cx", function(d,i) {
+                return w/2 + (offset + mainR + ((d.dist-min)/(max-min))*distScale)*Math.cos(2*i*Math.PI/neighbors.length);
+            })
+        .attr("cy", function(d,i) {
+                return h/2 - (offset + mainR + ((d.dist-min)/(max-min))*distScale)*Math.sin(2*i*Math.PI/neighbors.length);
+            })
+        .attr("r", function(d) {
+                return d.wins*10+10;
+            })
+        .attr("fill", function(d,i) {
+                return (d.name in colors) ? colors[d.name] : "#000000";
+            })
+        .moveToFront()
+        .on("mouseover",function() {
+                color= d3.select(this).attr("fill");
+                d3.select(this).attr("stroke",d3.rgb(color).darker(1.5));
+                d3.select(this).attr("stroke-width","5");
+            })
+        .on("mouseout",function() {
+                d3.select(this).attr("stroke-width","0");
+            })
+        .on("click", function() {
+                resize(d3.select(this.parentNode));
+            });
+        
+    //add text to each group, set properties and event handler
+    orbit.select("text.orbit")
+        .attr("x", function(d,i) {
+                return w/2 + (offset + mainR + ((d.dist-min)/(max-min))*distScale)*Math.cos(2*i*Math.PI/neighbors.length);
+            })
+        .attr("y", function(d,i) {
+                return h/2 - (offset + mainR + ((d.dist-min)/(max-min))*distScale)*Math.sin(2*i*Math.PI/neighbors.length);
+            })
+        .attr("text-anchor", "middle")
+        .attr("dominant-baseline","central")
+        .attr("fill", "white")
+        .attr("visibility","hidden")
+        .text(function(d) { return teamText(d); })
+        .on("click", function() {
+                if(d3.select(this).attr("visibility")=="visible") {
+                    resize(d3.select(this.parentNode));
+                }
+            });		
+            
 }
