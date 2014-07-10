7c1115950af8a40673d2194ac17b92f4a52419a9
nikhilNathwani <njn27@cornell.edu>
2014-07-10 02:45:56 -0400

    Modified allDataToJSON and helper functions so that win shares and salaries are included in the json. This was a big deal\!

--- quick/winShare.py
+++ quick/winShare.py
@@ -38,12 +38,14 @@ def scrapeWinShares(url):
 	if salaryTable!=None:	
 		rows= salaryTable.findAll("tr")
 		for row in rows:
-			player= row.find("a").text.encode("ascii","ignore")
-			if player in stats:
-				salary= row.findAll("td")[-1].text
-				salary= int(salary.replace(",","").replace("$",""))
-				stats[player].append(salary)
-				totalSalary += salary
+			link= row.find("a")
+			if link!=None:
+				player= row.find("a").text.encode("ascii","ignore")
+				if player in stats:
+					salary= row.findAll("td")[-1].text
+					salary= int(salary.replace(",","").replace("$",""))
+					stats[player].append(salary)
+					totalSalary += salary
 
 		#add salary percentages to dicts
 		totalSalary= float(totalSalary)
@@ -56,9 +58,6 @@ def scrapeWinShares(url):
 				stats[p]= stats[p][:2]+[sal]
 			if len(stats[p])==3:
 				stats[p].append((sal/totalSalary)*100)
-
-	else:
-		print yearFromURL(url)
 
 	#convert stats dict to list of lists
 	stats= [[x]+y for x,y in stats.items()]
@@ -121,22 +120,37 @@ def getWinSharesForTeam(team,year):
 	lists= csvToLists(currentDir+'/'+team+'/'+str(year)+'.csv')
 	return lists
 
+#returns dict where keys are best players and values are stat lists
 def getBestWinSharePlayers(fn):
 	currentDir= os.getcwd()
-	playerTups= csvToLists(fn)
+	playerLists= csvToLists(fn)
 
 	total= 0
-	for (p,ws) in playerTups:
-		total += float(ws)
+	for x in playerLists:
+		total += float(x[1])
 
-	bestToWorst= sorted(playerTups, key=lambda tup: float(tup[1]), reverse=True)
+	bestToWorst= sorted(playerLists, key=lambda tup: float(tup[1]), reverse=True)
 	bestPlayers= {"all":total}	
 	tally= 0
-	for (p,ws) in bestToWorst:
-		tally += float(ws) 
-		bestPlayers[p]= float(ws)
+	for x in bestToWorst:
+		tally += float(x[1]) 
+		bestPlayers[x[0]]= [float(elem) for elem in x[1:]]
 		if tally >= total/2:
 			return bestPlayers
+
+def getStatLine(team,player,year):
+	urls= getURLsOfTeam(team)
+	url= ""
+	for u in urls:
+		if str(year) in u:
+			url= u
+	currentDir= os.getcwd()
+	lists= csvToLists((currentDir + '/' + team + '/' + urlToFilename(url)))
+	info= {}
+	for lst in lists:
+		if lst[0]==player:
+			return (lst[0],lst[1:])
+	return (player,[-1,-1,-1,-1])
 
 def calcEras(team):
 	currentDir= os.getcwd()
@@ -145,34 +159,55 @@ def calcEras(team):
 	currYear= 0
 	eras= {}
 	possibles= {}
+	playerYears= {}
 
 	for url in urls:
 		currYear= int(yearFromURL(url))
+
+		#bests= {player : [stats]}
 		bests= getBestWinSharePlayers(currentDir + '/' + team + '/' + urlToFilename(url))
 		bests= [(k,v) for (k,v) in bests.iteritems() if k != "all"]
-		bests= sorted(bests, key=lambda tup: float(tup[1]), reverse=True)
-		eras[currYear]= set([bests[0]])
-		possibles[currYear]= set(bests[:4])
-		#print bests, bests[:3]
+		bests= sorted(bests, key=lambda tup: float(tup[1][0]), reverse=True)
+
+		#best is now a list of (player, [stats]) tuples sorted by win shares
+
+		#eras= {year : set([(players,[stats])])}
+		eras[currYear]= [bests[0]]
+		playerYears[bests[0][0]]= playerYears.get(bests[0][0],[]) + [currYear]
+		possibles[currYear]= bests[:4] #contains top three players as opposed to just the best
+		for b in bests[:4]:
+			playerYears[b[0]]= playerYears.get(b[0],[])
 
 		if (currYear-2 in possibles):
+			#tmp= list of names of top 3 players of currYear-2
+			tmp= [p for (p,lst) in possibles[currYear-2]]
 			for player in possibles[currYear]:
-				tmp= [p for (p,ws) in possibles[currYear-2]]
-				if (player[0] in tmp): #or (player in possibles[currYear-1]):
-					eras[currYear-2].add(player)
-					eras[currYear-1].add(player)
-					eras[currYear].add(player)
+				if (player[0] in tmp): #or (player[0] in possibles[currYear-1]):
+					for j in range(3):
+						if currYear-j not in playerYears[player[0]]:
+							eras[currYear-j].append(getStatLine(team,player[0],currYear-j))
+							playerYears[player[0]].append(currYear-j)
 
+	#eras= [(year,[(player,[stats]),(),...,()])] <-- sorted by year
 	eras= sorted(eras.items(), key=lambda tup: int(tup[0]))
 	collapsed= []
 	byPlayer= {}
-	yearSpan= [eras[0][0]]
-	currPlayers= sorted(list(set([p for (p,ws) in eras[0][1]])))
-	for yr,p in eras:
-		playerList= sorted(list(set([y for (y,ws) in p])))
-		for play in playerList:
-			byPlayer[play]= byPlayer.get(play,[]) + [yr]
-		if playerList==currPlayers:
+	#yearSpan= [eras[0][0]]
+	#currPlayers= alphabetically sorted list of players in first year
+	#currPlayers= sorted(list(set([p for (p,ws) in eras[0][1]])))
+	for yr,playerStats in eras:
+		print playerStats
+		for player,stats in playerStats:
+			season= '\''+str(yr-1)[-2:]+'-\''+str(yr)[-2:]
+			print yr, playerStats, player,stats
+			info= {"name":player,"year":yr,"season":season}
+			info["winShares"]= stats[0]
+			info["winShare%"]= stats[1]
+			if len(stats)>2:
+				info["salary"]= stats[2]
+				info["salary%"]= stats[3]
+			byPlayer[player]= byPlayer.get(player,[]) + [info]
+	'''	if playerList==currPlayers:
 			yearSpan.append(yr)
 		else:
 			collapsed.append({"years":yearSpan, "players":currPlayers})
@@ -180,15 +215,13 @@ def calcEras(team):
 			currPlayers= playerList
 	collapsed.append({"years":yearSpan, "players":currPlayers})
 	for x in collapsed:
-		print x
+		print x'''
 
-	for x,y in sorted(byPlayer.items(), key=lambda tup: tup[1][0]):
+	for x,y in sorted(byPlayer.items(), key=lambda tup: tup[1][0]["year"]):
 		print x,y
 
-	return sorted(byPlayer.items(), key=lambda tup: tup[1][0])
+	return sorted(byPlayer.items(), key=lambda tup: tup[1][0]["year"])
 
-def getPlayerSeasonStats(player,year):
-	pass
 
 def allDataToJSON():
 	currentDir= os.getcwd()
@@ -200,11 +233,8 @@ def allDataToJSON():
 		info= {"team":team, "eras": [], "confFinal": [], "lossFinal": [], "champion": []}
 		print team
 		eras= calcEras(team)
-		for (p,yrs) in eras:
-			info["eras"].append({"player":p, "stats":[]})
-			for y in yrs:
-				season= '\''+str(y-1)[-2:]+'-\''+str(y)[-2:]
-				info["eras"][-1]["stats"].append({"name":p, "year":y, "season":season})
+		for (p,stats) in eras:
+			info["eras"].append({"player":p, "stats":stats})
 		cf,lf,c= getSuccessfulYears(team)
 		info["confFinal"]= cf
 		info["lossFinal"]= lf
@@ -218,5 +248,5 @@ def allDataToJSON():
 if __name__ == "__main__":
 	start= time.time()
 	teams= ["ATL", "BOS", "BRK", "CHA", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOP", "NYK", "OKC", "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"]
-	recordWinSharesForTeam("ATL")
+	allDataToJSON()
 	print "Time taken:", time.time()-start
